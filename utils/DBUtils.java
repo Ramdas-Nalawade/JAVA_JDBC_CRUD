5f8785168a77f0db3fdef38411d9a8b473aba3c5
Ramdas-Nalawade <147508047+Ramdas-Nalawade@users.noreply.github.com>
2023-12-07 07:22:07 -0800

    Add files via upload

---
+++ utils/DBUtils.java
@@ -0,0 +1,26 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtils 
+{
+	public static Connection connection;
+	
+	public static Connection openConnection() throws SQLException
+	{
+		String url = "jdbc:mysql://localhost:3306/advjava?createDatabaseI"
+				+ "fNotExist=true&useSSL=false&allowPublicKeyRetrieval=true";
+		connection = DriverManager.getConnection(url, "root", "root123");
+		
+		return connection;
+	}
+	public static void closeConnection() throws SQLException
+	{
+		if(connection != null)
+		{
+			connection.close();
+		}
+	}
+}
